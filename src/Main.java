dedae769547054fccce62db01042b1a1a2abf46b
msx94 <maxime.quentin@etu.enseeiht.fr>
2016-08-09 16:23:32 +0100

    upgrade

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Instanciation of a Window Object
+		//Instantiation of a Window Object
 		Window window = new Window();
 		window.build();
 		window.setResizable(true);
